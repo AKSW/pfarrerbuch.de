f040973fab0feb355966c7bf7a48307ca01f1643
Simeon Ackerman <amseon@web.de>
2014-11-07 10:42:49 +0100

    Remove depreciated local inputs and add owrdform.js

--- site/js/site.js
+++ site/js/site.js
@@ -1,22 +1,42 @@
+
+
+var owCon = new OntoWikiConnection(urlBase + 'jsonrpc');
+
 $("#rdform-edit-btn").click(function() {
-
-	//$("body").append('<div><form class="test-form"><input type="text" /><input type="submit" /></form></div>');
-	$("body").append('<div class="rdform-container"><form class="rdform form-horizontal" onsubmit="return false;"></form></div>');
-	$(".rdform").prepend('<p><button class="btn btn-sm btn-close pull-right" alt="Close title="Close">x</button></p>');	
-	$(".rdform-container").hide();
-
-	var con = new OntoWikiConnection(urlBase + 'jsonrpc');
-	con.getResource( modelIri, resourceIri );
-	$(".rdform-container").show();
-
-	$(document).keyup(function(e) {
-	  if (e.keyCode == 27) { window.location.reload(); }   // esc
-	});
-
-	return false;
+		var container = $('<div class="rdform-container"></div>');
+		$("body").append(container);
+		var resourceIri = $(this).attr("data-resourceIri");
+		
+		owCon.getResource( modelIri, resourceIri, function( resData ) {
+			var owRdform = new OntoWikiRDForm();
+			owRdform.$container = container;
+			//resData.resourceIri = resourceIri;
+			
+			owRdform.initForm(resourceIri, resData, function( result ) {
+				var hash = owRdform.$elem.children('#dataHash').val();
+                
+                owCon.updateResource( modelIri, resourceIri, hash, result, function( updateResult ) {
+                	window.location.href = decodeURIComponent(resourceIri);
+                } );
+			});
+			owRdform.$elem.prepend('<p><button class="btn btn-sm close-rdform-btn pull-right" alt="Close title="Close">x</button></p>');	
+		});
+		
+		return false;
 });
 
+// close the current form window
+$("body").on("click", ".close-rdform-btn", function() {
+	//reload window to re-initial all files
+	//var redirectUri = $("#redirectUri").val();
+	//window.location.href = decodeURIComponent(redirectUri);
+	$(this).parentsUntil(".rdform-container").parent().remove();
+	return false;
+})
+/*
 $("#rdform-new-btn").click(function() {
+
+	//var newResourceId = 
 
 	$("body").append('<div class="rdform-container"><form class="rdform form-horizontal"</form></div>');
 	$(".rdform-container").hide();
@@ -26,21 +46,17 @@ $("#rdform-new-btn").click(function() {
 	$('#resourceIri').val('http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999');
 	$('#redirectUri').val('http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999');
 
+	$("#resourceId").val( '{http://xmlns.com/foaf/0.1/name}' );
+
 	// TODO: dataHash kommt aus leerer Resource!
 	$('#dataHash').val('');
 
 	var con = new OntoWikiConnection(urlBase + 'jsonrpc');
-	con.getResource( modelIri, 'http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999' );
+	//con.getResource( modelIri, 'http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999' );
+	con.getResource( modelIri, resourceIri );
 	//con.initRDForm( undefined );
 	$(".rdform-container").show();
 
 	return false;
 });
-
-$("body").on("click", ".btn-close", function() {
-	//reload window to re-initial all files
-	window.location.reload();
-
-	//$(".rdform-container").remove();
-	return false;
-})
+*/
