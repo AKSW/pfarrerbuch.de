92bf681a563cf74b1fd388bce38083332c62562f
Simeon Ackerman <amseon@web.de>
2014-11-07 14:28:43 +0100

    Implement new resource button opens new empty form

--- site/js/site.js
+++ site/js/site.js
@@ -2,22 +2,51 @@
 
 var owCon = new OntoWikiConnection(urlBase + 'jsonrpc');
 
+// click edit btn
 $("#rdform-edit-btn").click(function() {
 		var container = $('<div class="rdform-container"></div>');
 		$("body").append(container);
 		var resourceIri = $(this).attr("data-resourceIri");
 		
 		owCon.getResource( modelIri, resourceIri, function( resData ) {
+			var hash = resData.dataHash;
 			var owRdform = new OntoWikiRDForm();
 			owRdform.$container = container;
-			//resData.resourceIri = resourceIri;
 			
 			owRdform.initForm(resourceIri, resData, function( result ) {
-				var hash = owRdform.$elem.children('#dataHash').val();
+				//var hash = owRdform.$elem.children('#dataHash').val();
                 
                 owCon.updateResource( modelIri, resourceIri, hash, result, function( updateResult ) {
                 	window.location.href = decodeURIComponent(resourceIri);
                 } );
+			});
+			owRdform.$elem.prepend('<p><button class="btn btn-sm close-rdform-btn pull-right" alt="Close title="Close">x</button></p>');	
+		});
+		
+		return false;
+});
+
+// click new btn
+$("#rdform-new-btn").click(function() {
+		var container = $('<div class="rdform-container"></div>');
+		$("body").append(container);
+
+		var now = new Date();
+		var pID = now.getTime();
+		var resourceIri = 'http://pfarrerbuch.comiles.eu/sachsen/person/-'+pID;
+
+		owCon.getResource( modelIri, resourceIri, function( resData ) {
+			var hash = resData.dataHash;
+			var owRdform = new OntoWikiRDForm();
+			owRdform.$container = container;
+			owRdform.template = "Person";
+			
+			owRdform.initForm(resourceIri, resData, function( result ) {					
+				var resultId = result["@id"];
+	            
+	            owCon.updateResource( modelIri, resourceIri, hash, result, function( updateResult ) {
+	            	window.location.href = decodeURIComponent(resourceIri);
+	            } );
 			});
 			owRdform.$elem.prepend('<p><button class="btn btn-sm close-rdform-btn pull-right" alt="Close title="Close">x</button></p>');	
 		});
@@ -33,30 +62,3 @@ $("body").on("click", ".close-rdform-btn", function() {
 	$(this).parentsUntil(".rdform-container").parent().remove();
 	return false;
 })
-/*
-$("#rdform-new-btn").click(function() {
-
-	//var newResourceId = 
-
-	$("body").append('<div class="rdform-container"><form class="rdform form-horizontal"</form></div>');
-	$(".rdform-container").hide();
-
-	$('#editable').prop('checked', true);
-	// TODO: generate resourceId
-	$('#resourceIri').val('http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999');
-	$('#redirectUri').val('http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999');
-
-	$("#resourceId").val( '{http://xmlns.com/foaf/0.1/name}' );
-
-	// TODO: dataHash kommt aus leerer Resource!
-	$('#dataHash').val('');
-
-	var con = new OntoWikiConnection(urlBase + 'jsonrpc');
-	//con.getResource( modelIri, 'http://pfarrerbuch.comiles.eu/sachsen/person/-9999999999' );
-	con.getResource( modelIri, resourceIri );
-	//con.initRDForm( undefined );
-	$(".rdform-container").show();
-
-	return false;
-});
-*/
